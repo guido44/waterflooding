13c66309ea7f395a6e9442dbd2ddd2f52d2652f4
guido44 <guido.aldas@epn.edu.ec>
2020-10-12 19:25:01 -0500

    Corrections on Brooks-Corey relations for relative permeabilities
    Changed the equations for relative permeabilities. Some other corrections and tips on the code.

--- obtener_kro.py
+++ obtener_kro.py
@@ -2,14 +2,23 @@ import numpy as np
 import pandas as pd
 
 
-def obtener_kro(swi, sor, n):
-    sw = np.linspace(swi, 1 - sor, n)
-    swd = (sw - swi) / (1 - swi - sor)
-    kro = ((1-swd)**2)*(1-swd**2)
-    matriz_kro = np.array([sw, kro])
-    dataframe1 = pd.DataFrame(matriz_kro)
-    dataframe1 =dataframe1.T
-    print(dataframe1)
+def calculate_kro(swc, sor, kromax, no,  n):
+    """
+    This function calculates n values of Kro using the modified Brooks-Corey relations
+    :param swc: This is the critical saturation of water
+    :param sor: This is the residual saturation of oil
+    :param kromax: This is the maximum relative permeability for oil between 0 and 1
+    :param no: This is the oil exponent for modified Brooks-Corey relations range range from 1 to 6
+    :param n: This is the number of rows calculated in the DataFrame
+    :return: This will return a DataFrame with n Sw and Kro values
+    """
+    sw = np.linspace(swc, 1 - sor, n)
+    # Modified Brooks-Corey relation for Kro
+    kro = kromax * ((1 - sw - sor) / (1 - sor - swc)) ** no
+    # Creation of the result
+    #result = {'Sw': sw, 'kro': kro}
+    #dataframe = pd.DataFrame(result)
     return kro
 
-#print(obtener_kro(0.363, 0.205, 20))
+
+#print(calculate_kro(0.363, 0.205, 1, 3, 20))
--- obtener_krw.py
+++ obtener_krw.py
@@ -2,14 +2,23 @@ import numpy as np
 import pandas as pd
 
 
-def obtener_krw(swi, sor, n):
-    sw = np.linspace(swi, 1 - sor, n)
-    swd = (sw - swi) / (1 - swi - sor)
-    krw = swd ** 4
-    matriz_krw = np.array([sw, krw])
-    dataframe1 = pd.DataFrame(matriz_krw)
-    dataframe1 =dataframe1.T
-    print(dataframe1)
+def calculate_krw(swc, sor, krwmax, nw,  n):
+    """
+    This function calculates n values of Krw using the modified Brooks-Corey relations
+    :param swc: This is the critical saturation of water
+    :param sor: This is the residual saturation of oil
+    :param krwmax: This is the maximum relative permeability for water between 0 and 1
+    :param nw: This is the water exponent for modified Brooks-Corey relations range range from 1 to 6
+    :param n: This is the number of rows calculated in the DataFrame
+    :return: This will return a DataFrame with n Sw and Krw values
+    """
+    sw = np.linspace(swc, 1 - sor, n)
+    # Modified Brooks-Corey relation for Krw
+    krw = krwmax * ((sw - swc) / (1 - sor - swc)) ** nw
+    # Creation of the result
+    #result = {'Sw': sw, 'krw': krw}
+    #dataframe = pd.DataFrame(result)
     return krw
 
-#print(obtener_krw(0.363, 0.205, 20))
+
+#print(calculate_krw(0.363, 0.205, 1, 3, 20))
