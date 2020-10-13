8cec6b888ed8dcc179c23139e1896cc286f75525
guido44 <guido.aldas@epn.edu.ec>
2020-10-12 19:39:21 -0500

    Corrections for the fractional flow function
    Created the guidelines for the code. Did some other corrections.

--- task2.py
+++ task2.py
@@ -1,28 +1,30 @@
-import numpy as np
-import pandas as pd
-import math
 from obtener_kro import *
 from obtener_krw import *
 
 
-def obtener_fw(swi, sor, uo, uw, n):
-    sw = np.linspace(swi, 1 - sor, n)
-    print("Tabla Kro")
-    kro = obtener_kro(swi, sor, n)
-    print("Tabla Krw")
-    krw = obtener_krw(swi, sor, n)
-    fw = 1/(1+((kro*uw)/(krw*uo)))
-    matriz_fw = np.array([sw,fw])
-    dataframe = pd.DataFrame(matriz_fw)
-    dataframe = dataframe.T
-    from matplotlib import pyplot as plt
-    plt.plot(sw, fw)
-    plt.xlabel("Sw")
-    plt.ylabel("fw")
-    plt.show()
-    print("Tabla fw")
+def calculate_fw(swc, sor, kromax, krwmax, no, nw, uo, uw, n):
+    """
+    This function calculates the fractional flow using the relative permeabilities of the rock
+    :param swc: This is the critical saturation of water
+    :param sor: This is the residual saturation of oil
+    :param kromax: This is the maximum relative permeability for oil between 0 and 1
+    :param krwmax: This is the maximum relative permeability for water between 0 and 1
+    :param no: This is the oil exponent for modified Brooks-Corey relations range range from 1 to 6
+    :param nw: This is the water exponent for modified Brooks-Corey relations range range from 1 to 6
+    :param uo: This is the oil viscosity given in cp
+    :param uw: This is the water viscosity given in cp
+    :param n: This is the number of rows calculated in the DataFrame
+    :return: This will return a DataFrame with n Sw and fw values
+    """
+    sw = np.linspace(swc, 1 - sor, n)
+    kro = calculate_kro(swc, sor, kromax, no, n)
+    krw = calculate_krw(swc, sor, krwmax, nw, n)
+    fw = 1 / (1 + ((kro * uw) / (krw * uo)))
+    result = {'Sw:': sw, 'fw:': fw}
+    dataframe = pd.DataFrame(result)
     return dataframe
 
-#print(obtener_fw(0.365, 0.205, 2, 1, 20))
+
+#print(calculate_fw(0.363, 0.205, 1, 1, 3, 3, 2, 1, 20))
 
 
