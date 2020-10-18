a56c372368e30ce6cd7179d1e97329ad90dcca8e
guido44 <guido.aldas@epn.edu.ec>
2020-10-18 17:57:52 -0500

    Added Vogel equation
    In this commit I added the Vogel equation considering the bubble point pressure. So, when the bottomhole pressure is greater than the bubble pressure the code will use the pseudo-steady state Darcy equation. Otherwise, it will use the Vogel equation.
    Finally, I did some corrections on the comments and created  other code lines.

--- pseudo_steady_state.py
+++ pseudo_steady_state.py
@@ -3,9 +3,9 @@ import pandas as pd
 import math
 
 
-def obtener_q(pwf, k, h, pr, uo, bo, re, rw, s):
+def calculate_q(pwf, k, h, pr, uo, bo, re, rw, s, pb):
     """
-    Thi is a function to calculate the rates based on the pseudo-steady state Darcy equation
+    Thi is a function to calculate the rates based on the pseudo-steady state Darcy equation and the Vogel equation
     :param pwf: Single-value or array of bottomhole pressures
     :param k: This is the effective permeability to the phase of analysis
     :param h: This is the reservoir thickness that contributes to the total flow
@@ -15,16 +15,40 @@ def obtener_q(pwf, k, h, pr, uo, bo, re, rw, s):
     :param re: This is the external boundary radius
     :param rw: This is the wellbore radius
     :param s: This is the skin factor
+    :param pb: This is the bubble point pressure
     :return: This will return a float if the pwf is a float or a np.array if pwf is an array
     """
+
     pwf = np.array(pwf)
-    # Pseudo-steady state Darcy equation to calculate the rate of the analyzed phase
-    q = (0.00708 * k * h * (pr - pwf)) / (uo * bo * (s - 0.75 + math.log(re / rw)))
+    # pwfd is an array with all the values of pressure that are greater than the bubble point pressure
+    pwfd = pwf > pb
+    # pwfv is an array with all the values of pressure that are lees than or equal the bubble point pressure
+    pwfv = pwf <= pb
+
+    # %%
+    # Pseudo-steady state Darcy equation to calculate the rate of the analyzed phase when
+    # the bottomhole pressure is greater than the bubble point pressure
+
+    # qd is an array with the rates when the bottomhole pressure is greater than the bubble point pressure
+    qd = (0.00708 * k * h * (pr - pwf[pwfd])) / (uo * bo * (s - 0.75 + math.log(re / rw)))
+
+    # %%
+    # Vogel equation to calculate the rate of the analyzed phase when the bottomhole pressure is less than the bubble
+    # point pressure
+
+    # qb means the rate calculated at the bubble point pressure
+    qb = (0.00708 * k * h * (pr - pb)) / (uo * bo * (s - 0.75 + math.log(re / rw)))
+    # qomax is the maximum rate when the bottomhole pressure is 0
+    qomax = qb / (1 - 0.2 * (pb/pr) - 0.8 * (pb/pr) ** 2)
+    # qv is an array with the rates when the bottomhole pressure is less than the bubble point pressure
+    qv = qomax * (1 - 0.2 * (pwf[pwfv]/pr) - 0.8 * (pwf[pwfv]/pr) ** 2)
+    # %%
 
     # Creation of result data
+    q = np.concatenate((qd,qv))
     result = {'rate': q, 'bottomhole pressure': pwf}
     dataframe = pd.DataFrame(result)
     return dataframe
 
 
-print(obtener_q([1000], 10, 30, 2000, 10, 1.2, 500, 0.244, 3))
+#print(calculate_q([3200, 3000, 2800, 2000, 1800, 1000, 200, 0],30, 30, 3200, 1.2, 1.2, 2000, 0.244, 2, 1800))
